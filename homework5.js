103f80daef8be05c5c799a5ebeaa47b370111af4
Ivan Yeremenko <ivan.yeremenko@coxautoinc.com>
2024-02-07 14:33:26 +0200

    Homework #5

---
+++ homework5.js
@@ -0,0 +1,5 @@
+const arr = [false, 12, '23424', false, 324, '', true, null, 5, -0, 0, 9, undefined, NaN, 6.354, undefined, 7, null];
+
+const filteredArr = arr.filter(Boolean);
+
+console.log(filteredArr);
