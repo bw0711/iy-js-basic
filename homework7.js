e9a54ef8ad0c7ffd42929caef0ce3eb18383a5e9
Ivan Yeremenko <ivan.yeremenko@coxautoinc.com>
2024-02-09 11:57:40 +0200

    Homework for lesson 7

---
+++ homework7.js
@@ -0,0 +1,9 @@
+const calculate = (num1, num2) => {
+    return (num1 % 2 === 0 && num2 % 2 === 0) ? num1 * num2 :
+           (num1 % 2 !== 0 && num2 % 2 !== 0) ? num1 + num2 :
+           num1 % 2 !== 0 ? num1 : num2;
+};
+
+console.log(calculate(6, 8));
+console.log(calculate(3, 5));
+console.log(calculate(3, 8));
