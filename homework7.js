7ff3a4262ebb0f7b69947005fc64bd78f5442144
Ivan Yeremenko <ivan.yeremenko@coxautoinc.com>
2024-02-09 16:36:27 +0200

    minor fixes

--- homework7.js
+++ homework7.js
@@ -1,8 +1,8 @@
-const calculate = (num1, num2) => {
-    return (num1 % 2 === 0 && num2 % 2 === 0) ? num1 * num2 :
-           (num1 % 2 !== 0 && num2 % 2 !== 0) ? num1 + num2 :
-           num1 % 2 !== 0 ? num1 : num2;
-};
+const calculate = (num1, num2) => 
+  num1 % 2 === 0 && num2 % 2 === 0 ? num1 * num2 :
+  num1 % 2 !== 0 && num2 % 2 !== 0 ? num1 + num2 :
+  num1 % 2 !== 0 ? num1 : num2;
+
 
 console.log(calculate(6, 8));
 console.log(calculate(3, 5));
