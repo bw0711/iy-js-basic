1f23f26158b6e14306637213155d1fe570e79eb2
Ivan Yeremenko <ivan.yeremenko@coxautoinc.com>
2024-03-04 17:07:04 +0200

    fixes

--- homework11.js
+++ homework11.js
@@ -1,14 +1,12 @@
-const ConstructPerson = (name, age, gender, occupation) => {
-    return {
-        name,
-        age,
-        gender,
-        occupation
-    };
-};
+function ConstructPerson(name, age, gender, occupation) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.occupation = occupation;
+}
 
-const person1 = ConstructPerson("John Doe", 30, "Male", "Engineer");
-const person2 = ConstructPerson("Jane Smith", 25, "Female", "Doctor");
+const person1 = new ConstructPerson("John Doe", 30, "Male", "Engineer");
+const person2 = new ConstructPerson("Jane Smith", 25, "Female", "Doctor");
 
 console.log(person1);
 console.log(person2);
